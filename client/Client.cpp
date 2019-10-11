c43ebe557f4a2619fdafd06121566e9c49adfd63
Lucas <lucas.tabis@epitech.eu>
2019-10-11 22:08:04 +0200

    [minor] file : Changed file names + minimal debug output changed.

--- client/Client.cpp
+++ client/Client.cpp
@@ -5,7 +5,6 @@
 #include "Client.hpp"
 
 #include <QtCore/QDebug>
-#include <server/server.hpp>
 
 GuessGame::Client::Client(const QUrl &url, bool debug, QObject *parent) :
 QObject(parent),
@@ -13,7 +12,7 @@ _url(url),
 _debug(debug)
 {
     if (_debug)
-        qDebug() << "WebSocket server:" << url;
+        qDebug() << "[Client] WebSocket server:" << url;
     connect(&_webSocket, &QWebSocket::connected, this, &Client::onConnected);
     connect(&_webSocket, &QWebSocket::disconnected, this, &Client::closed);
     _webSocket.open(QUrl(url));
@@ -22,15 +21,15 @@ _debug(debug)
 void GuessGame::Client::onConnected()
 {
     if (_debug)
-        qDebug() << "WebSocket connected";
+        qDebug() << "[Client] WebSocket connected";
     connect(&_webSocket, &QWebSocket::textMessageReceived,
             this, &Client::onTextMessageReceived);
-    _webSocket.sendTextMessage(QStringLiteral("Hello, world!"));
+    _webSocket.sendTextMessage(QStringLiteral("connexion")); // A mettre en JSON
 }
 
 void GuessGame::Client::onTextMessageReceived(QString message)
 {
     if (_debug)
-        qDebug() << "Message received:" << message;
+        qDebug() << "[Client] Message received:" << message;
     _webSocket.close();
 }
--- server/main_server.cpp
+++ server/main_server.cpp
@@ -4,7 +4,7 @@
 
 #include <memory>
 #include "Exception.hpp"
-#include "server.hpp"
+#include "Server.hpp"
 
 int main(int ac, char *av[])
 {
